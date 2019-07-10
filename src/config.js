a77580d75a126224be67826291720d312f8f0bf4
Erin Dustin <erincdustin@gmail.com>
2019-07-10 09:35:07 -0700

    changed auth endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: 'http://localhost:8000/api',
-  AUTH_ENDPOINT: 'http://localhost:8888/login'
+  // AUTH_ENDPOINT: 'http://localhost:8888/login'
+  AUTH_ENDPOINT: 'https://glacial-ridge-65782.herokuapp.com/login'
 }
